81c73c4ccf53849322a3d204f4d6d72593679f3c
Abhishek Kumar <bipulkumar469.ak@gmail.com>
2023-11-20 21:46:31 +0530

    Inception

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello World from ReactJs!"
+);
+console.log(heading.props);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+root.render(parent);
